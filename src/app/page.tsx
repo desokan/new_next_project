d4a79e8c6fa69542f350dc9ce049af24fb024f40
desokan <thefdmm@gmail.com>
2025-02-26 08:55:29 +0000

    applied const assertion to the data file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ const HomePage = () => {
   return (
     <div>
       <h2 className="text-lg">HomePage</h2>
-      <Link href={ticketsPath()} className="text-sm underline">Go to Tickets Page</Link>
+      <Link href={ticketsPath()} className="text-sm underline">
+        Go to Tickets Page
+      </Link>
     </div>
   )
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,19 @@ import Link from 'next/link'
 import { initialTickets } from '@/data'
 import { ticketPath } from '@/path'
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  IN_PROGRESS: '>',
+  DONE: 'X',
+}
+
 const TicketsPage = () => {
   return (
     <div>
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{ticket.status}</div>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-lg">{ticket.title}</h2>
           <Link href={ticketPath(ticket.id)} className="text-sm underline">
             View
